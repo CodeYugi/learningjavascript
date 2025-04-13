d3003d69479fa26098396cec14d023a40b41bcbb
Yugi <yugiicode@gmail.com>
2025-04-13 15:06:07 +0000

    Updated

---
+++ basic/datatype.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as new version
+
+// alert("3+3")  we use node.js not browser
+
+console.log(3
+    +3) // code should be realable 
+
+console.log("yugi")
+
+
+// let name = raju 
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint => large number
+// string => ""
+// boolean => true/false
+// null => sandalone value
+// undefined => value is not assigned
+//symbol => unique
+
+// object 
+
+
+console.log(typeof true);
---
+++ basic/variable.js
@@ -0,0 +1,18 @@
+const accountId = 7777
+let accountEmail = "yugi@google.com" 
+var accountPass = "yugityagi"
+accountCity = "Modinagar"
+ let accountEmpty;
+//  accountLetsee;
+
+accountEmail = "xyz@gmail.com" // changing the value to see what happen //
+accountPass = "password"   // changing the value to see what happen //
+accountCity = "rajnagar"     // changing the value to see what happen //
+
+  console.log(accountId);   // Const varible not allowed //
+
+// console.table([accountEmail , accountCity , accountPass , accountId , accountEmpty] );
+console.log(accountEmpty);
+
+// perfer not to use var because of issue in block scope and functional scope //
+
