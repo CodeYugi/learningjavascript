1b059cf984fbef1b25c5b892837d80f418c7e452
Yugi <yugiicode@gmail.com>
2025-04-18 11:00:31 +0000

    updated

---
+++ basic/ConversionOperation.js
@@ -0,0 +1,72 @@
+// let score = "yugi"
+// console.log(typeof score);
+// console.log(typeof (score) );
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// "33" => 33
+// "82yugu" => NaN
+// true => 1
+// false => 0
+
+// let isLoggedIn = 1
+
+// let BooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn); // number 
+// console.log(isLoggedIn) // 1
+// console.log(BooleanIsLoggedIn)//  true 
+// console.log(typeof BooleanIsLoggedIn)// Boolean
+
+
+// let isLoggedIn = ""
+
+// let BooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn); // String
+//console.log(isLoggedIn) // 
+//console.log(BooleanIsLoggedIn)//  false
+// console.log(typeof BooleanIsLoggedIn)// Boolean
+
+
+// let isLoggedIn = "yugi"
+
+// let BooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn); // String
+// console.log(isLoggedIn) // yugi
+// console.log(BooleanIsLoggedIn)//  true
+// console.log(typeof BooleanIsLoggedIn)// Boolean
+
+
+
+// let numberis = 77
+
+// let numberisString = String(numberis)
+
+//console.log(numberis);
+//console.log(typeof numberis);
+//console.log(numberisString);
+//console.log(typeof numberisString);
+
+// ******************Operation********************* // 
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+//console.log(2+2);
+// console.log(4-2);
+//console.log(2*2);
+//console.log(3**2);
+//console.log(4/2);
+//console.log(2%5);
+
+let star = "code"
+let star1 = " yugi"
+let star2 = star + star1
+ // console.log(star2);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log( 1 + 2 + "3");
