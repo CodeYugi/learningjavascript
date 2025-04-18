c1976e01f9a9009c85c3fedc5071e60a75bbc2b5
Yugi <yugiicode@gmail.com>
2025-04-18 19:57:14 +0000

    comparision updated

--- basic/comparision.js
+++ basic/comparision.js
@@ -3,4 +3,20 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log();
+// console.log(null > 0);
+// console.log(null == 0); 
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0); always flase 
+
+
+// ===
+
+// console.log("2" == 2);
+// console.log("2" === 2);
+// console.log("2" === "3");
+// console.log("2" === "2");
+// console.log(true === false);
+// console.log(true === true);
+
